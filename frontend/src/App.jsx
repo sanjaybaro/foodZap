0d24a2a6e00ef12afe6820f91c24453375cdd80a
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-21 12:01:53 +0530

    header added with background image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Cart from "./Pages/Cart/Cart";
+import Home from "./Pages/Home/Home";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
