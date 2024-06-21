eb320e42f8b57aa68f91d7f0f17c00fc34cd7e19
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-21 23:00:14 +0530

    food display with categories also addeed

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,32 +1,33 @@
 import React from "react";
-import { useState } from "react";
+import { useContext } from "react";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../Context/StoreContext";
 import "./FoodItem.css";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
-        {!itemCount ? (
+        {!cartItems[id] ? (
           <img
             className="add"
-            onClick={() => setItemCount((prev) => prev + 1)}
+            onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             alt=""
           />
         ) : (
           <div className="food-item-counter">
             <img
-              onClick={() => setItemCount((prev) => prev - 1)}
+              onClick={() => removeFromCart(id)}
               src={assets.remove_icon_red}
               alt=""
             />
-            <p>{itemCount}</p>
+            <p>{cartItems[id]}</p>
             <img
-              onClick={() => setItemCount((prev) => prev + 1)}
+              onClick={() => addToCart(id)}
               src={assets.add_icon_green}
               alt=""
             />
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { createContext } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemID) => {
+    if (!cartItems[itemID]) {
+      //if that id not avilable create new one
+      setCartItems((prev) => ({ ...prev, [itemID]: 1 }));
+    } else {
+      //if available add prev +1
+      setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemID) => {
+    setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
   const contextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
   };
 
   return (
