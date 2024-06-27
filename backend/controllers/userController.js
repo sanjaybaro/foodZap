2c4b785ed3f74fefd0c35b7ed52d3a0050a34d4a
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-27 23:20:59 +0530

    login functionality added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,34 @@ import bcrypt from "bcrypt";
 import validator from "validator";
 
 //login user
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User Dosen't exist",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = craeteToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 //logic for creating token
 const craeteToken = (id) => {
