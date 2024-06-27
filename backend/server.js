c8d263d88d3266e8aaa43dae6c7a9bdd4cb063ab
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-27 22:00:15 +0530

    user register added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { connectionDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config.js'
 
 //app config
 const app = express();
@@ -22,6 +24,7 @@ connectionDB();
 //api endpoints
 app.use("/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/user",userRouter)
 
 //run express server on port
 app.listen(port, () => {
