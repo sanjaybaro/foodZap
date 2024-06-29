838988f5dc0ee4445bbd68a8aab31792d447b7ff
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-29 12:14:35 +0530

    shop cart functionality done for backend

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -4,11 +4,11 @@ import {
   removeFromCart,
   getCart,
 } from "../controllers/cartController.js";
-
+import authMiddleware from "../middleware/auth.js";
 const cartRouter = express.Router();
 
-cartRouter.post("/add", addToCart);
-cartRouter.post("/remove", removeFromCart);
-cartRouter.post("/get", getCart);
+cartRouter.post("/add", authMiddleware, addToCart);
+cartRouter.post("/remove", authMiddleware, removeFromCart);
+cartRouter.post("/get", authMiddleware, getCart);
 
-export default cartRouter
+export default cartRouter;
