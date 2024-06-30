a96027ed97222f227ec9767a7f8829e806288e1a
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-30 23:07:52 +0530

    user order logic in backend

--- backend/controllers/orderContoller.js
+++ backend/controllers/orderContoller.js
@@ -66,5 +66,15 @@ const verifyorder = async (req, res) => {
     res.json({ success: false, message: "Error" });
   }
 };
+//user orders for frontend
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
-export { placeOrder, verifyorder };
+export { placeOrder, verifyorder, userOrders };
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import { placeOrder, verifyorder } from "../controllers/orderContoller.js";
+import {
+  placeOrder,
+  userOrders,
+  verifyorder,
+} from "../controllers/orderContoller.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyorder);
+orderRouter.post("/userorders", authMiddleware, userOrders);
 
 export default orderRouter;
