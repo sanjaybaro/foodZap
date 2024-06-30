59f448f450b9e5e78ed1fbce1af0d80e615b59d2
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-30 12:35:56 +0530

    currency changes to usd

--- backend/controllers/orderContoller.js
+++ backend/controllers/orderContoller.js
@@ -19,21 +19,21 @@ const placeOrder = async (req, res) => {
 
     const line_items = req.body.items.map((item) => ({
       price_data: {
-        currency: "inr",
+        currency: "usd",
         product_data: {
           name: item.name,
         },
-        unit_amount: item.price * 100 * 80,
+        unit_amount: item.price * 100,
       },
       quantity: item.quantity,
     }));
     line_items.push({
       price_data: {
-        currency: "inr",
+        currency: "usd",
         product_data: {
           name: "Delivery Charges",
         },
-        unit_amount: 2 * 100 * 80,
+        unit_amount: 2 * 100,
       },
       quantity: 1,
     });
