0371adc084e6f741141895c9bb1bc774ec3fe9ab
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-30 21:06:41 +0530

    verify of payment page for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Cart from "./Pages/Cart/Cart";
 import Home from "./Pages/Home/Home";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Verify from "./Pages/Verify/Verify";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
         </Routes>
       </div>
       <Footer />
---
+++ frontend/src/Pages/Verify/Verify.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import "./Verify.css";
+import { url } from "../../utils/url";
+
+const Verify = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  const navigate = useNavigate();
+
+  const verifyPayment = async () => {
+    const response = await axios.post(url + "/order/verify", {
+      success,
+      orderId,
+    });
+    if (response.data.success) {
+      navigate("/myorders");
+    } else {
+      navigate("/");
+    }
+  };
+  useEffect(() => {
+    verifyPayment();
+  }, []);
+
+  return (
+    <div className="verify">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Verify;
