f748121439d0e9b27513f1571974337d09709a11
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-07-04 12:13:44 +0530

    update  db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connectionDB = async () =>{
- await mongoose.connect(process.env.mongoURL).
- then(()=>{
+export const connectionDB = async () => {
+  try {
+    await mongoose.connect(process.env.mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB Connected");
- })
-}
+  } catch (error) {
+    console.error("DB Connection Error:", error);
+    process.exit(1); // Exit process with failure
+  }
+};
