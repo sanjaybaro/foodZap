3a18025f7d6bcdeb8ca3817509f6a46a36e761df
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-07-04 12:16:35 +0530

    update backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,14 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectionDB = async () => {
-  try {
-    await mongoose.connect(process.env.mongoURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  await mongoose.connect(process.env.mongoURL).then(() => {
     console.log("DB Connected");
-  } catch (error) {
-    console.error("DB Connection Error:", error);
-    process.exit(1); // Exit process with failure
-  }
+  });
 };
